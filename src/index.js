18f94b19d966d2734615a6fdd80f34797efca4fc
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-11-12 16:13:55 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // adding tasks
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById('tasks')
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const taskInput = document.getElementById('new-task-description');
+    if (taskInput.value !== null) {
+    const li = document.createElement('li');
+    li.innerText = taskInput.value;
+    const btn = document.createElement('button');
+    btn.innerText = "🗑"
+    btn.className = "delete"
+    li.append(btn)
+    document.getElementById('tasks').append(li);
+    taskInput.value = null;
+    }
+  });
+
+  // Delete tasks
+  taskList.addEventListener('click', function(e){
+    console.log(e.target)
+    if (e.target.className === "delete") {
+      e.target.parentElement.remove();
+    }
+  })
 });
+
+
